aee3695b80e883566fce9f69707d4afe03091aec
cona <berkaycona@outlook.com>
2022-11-19 10:33:05 +0300

    App.js ve Home.js ayrımı yapıldı

---
+++ search-info/searchinfo/src/pages/Error.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { Link } from "react-router-dom";
+export default function Error() {
+  return <main>Errora</main>;
+}
---
+++ search-info/searchinfo/src/pages/Home.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import Data from "../data.json";
+import yt from "../icons/youtubeLogo.png";
+import { Link } from "react-router-dom";
+
+function Home() {
+  const [searchTerm, setSearchTerm] = useState("");
+  return (
+    <div className="container">
+      <h1>Information</h1>
+      <div className="inputContainer">
+        <input
+          className="search"
+          type="text"
+          placeholder="Enter"
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+      <div className="dataContainer">
+        {Data.filter((val) => {
+          if (searchTerm == "") return val;
+          //IT SEARCHES BY NAME AND ARTIST
+          else if (
+            val.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            val.Artist.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+            return val;
+        }).map((val) => {
+          return (
+            <div className="data" key={val.id}>
+              <img src={val.image} alt="" />
+              <h3>{val.title}</h3>
+              <h5>{val.Artist}</h5>
+              <h6>{val.Snippet1}</h6>
+              <h6>{val.Snippet2}</h6>
+              {/* Youtube Logo Start*/}
+              <a href={val.Link}>
+                <img id="ytlogo" src={yt} alt="" />
+              </a>
+              {/* Youtube Logo End*/}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
