8aa76d38179bf041be49b04a12c43cd3284dd082
cona <berkaycona@outlook.com>
2022-11-22 18:40:19 +0300

    nothing important

---
+++ search-info/searchinfo/src/pages/SingleArtistPage.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import Data from "../data.json";
+
+const SingleArtistPage = () => {
+  const { myyid } = useParams();
+  const [mydata, setmyData] = useState(
+    Data.find((mydata) => mydata.myyid === myyid)
+  );
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const myyitem = Data[myyid - 1];
+  return (
+    <div>
+      <p>{myyitem.Artist}</p>
+      <h3>{myyitem.title}</h3>
+      <h6>{myyitem.Snippet1}</h6>
+      <h6>{myyitem.Snippet2}</h6>
+    </div>
+  );
+};
+
+export default SingleArtistPage;
